8631c1bef87015d16f9f72ead0782f7874fcce94
Meatya <meatya96@gmail.com>
2024-02-08 15:28:26 +0600

    added title_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,10 @@
 def str_func(func):
     return func.upper()
+
+
+def title_func(var):
+    """
+    Функция делает заглавными первые буквы каждого слова в строке
+
+    """
+    return var.title()
